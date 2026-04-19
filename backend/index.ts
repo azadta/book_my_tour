8c04825c5b44602ab0c2af9b37353c165b7f7903
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-19 17:15:15 +0530

    initial setup on frontend and backend

---
+++ backend/index.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO as string)
+  .then((conn) => console.log(`Mongodb connected: ${conn.connection.host}`))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+const app = express();
+const PORT = 3000;
+app.use(
+  cors({
+    origin: "http//:localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});

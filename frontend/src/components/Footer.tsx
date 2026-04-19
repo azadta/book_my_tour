b8c93b341c8e842a195f8ef931e8b72650bbb758
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-19 22:14:37 +0530

    add footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,57 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 text-gray-200 pt-10 pb-6 ">
+      <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+        <div>
+          <h2 className="text-lg font-bold mb-4">Book My Tour</h2>
+          <p className="text-sm">
+            Discover handpicked travel experiences and unforgettable adventures
+            across the glob.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold mb-3">Explore</h3>
+          <ul className="space-y-2 text-sm">
+            <li className="hover:underline">Most Rated Desinations</li>
+            <li className="hover:underline">Offer Packages</li>
+            <li className="hover:underline">Provided Facilities</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-semibold mb-3">Support</h3>
+          <ul className="space-y-2 text-sm">
+            <li className="hover:underline">FAQs</li>
+            <li className="hover:underline">Help Center</li>
+            <li className="hover:underline">Cancellation Policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-semibold mb-3">Connected with us</h3>
+          <p>Email: support@bookmytour.com</p>
+          <p>Phone: +91 0000000000</p>
+          <div className="flex space-x-4 text-xl">
+            <a>
+              <FaFacebook className="hover:text-blue-500" />
+            </a>
+            <a>
+              <FaInstagram className="hover:text-blue-500" />
+            </a>
+            <a>
+              <FaTwitter className="hover:text-blue-500" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center text-sm mt-10 border-t border-gray-700 pt-4">
+        {new Date().getFullYear()} bookmytour@ All Rights Reserved
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

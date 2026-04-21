013f288f1c0961c960b94fd18cbdcf9868c7b46f
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-21 20:47:31 +0530

    add admin service

---
+++ backend/interfaces/IAdminService.ts
@@ -0,0 +1,70 @@
+import { IAdmin } from "../models/Admin.js";
+import { IDestination } from "../models/Destination.js";
+import { IOperator } from "../models/Operator.js";
+import { Ipackage } from "../models/Package.js";
+import { IPackageCategory } from "../models/PackageCategory.js";
+import { IUser } from "../models/User.js";
+
+export interface IAdminService {
+  loginAdminService(
+    email: string,
+    password: string,
+  ): Promise<{
+    accessToken: string;
+    refreshToken: string;
+    adminData: any;
+  }>;
+  updateAdminService(id: string, data: any): Promise<IAdmin | null>;
+  getOperatorVerificationRequestsService(): Promise<IOperator[]>;
+  verifyOperatorService(
+    id: string,
+    isVerified: boolean,
+  ): Promise<{
+    message: string;
+  }>;
+  getPaginatedOperatorsService(
+    skip: number,
+    limit: number,
+  ): Promise<IOperator[] | null>;
+  getTotalOperatorsCount(): Promise<number>;
+  getSingleOperatorService(id: string): Promise<IOperator | null>;
+  blockOperatorService(
+    id: string,
+    isBlocked: boolean,
+  ): Promise<IOperator | null>;
+  deleteOperatorService(id: string): Promise<IOperator | null>;
+  getPaginatedUsersService(
+    skip: number,
+    limit: number,
+  ): Promise<IUser[] | null>;
+  getTotalUsersCount(): Promise<number>;
+  getSingleUserService(id: string): Promise<IUser | null>;
+  blockUserService(id: string, isBlocked: boolean): Promise<IUser | null>;
+  createCategoryService(
+    data: Partial<IPackageCategory>,
+  ): Promise<IPackageCategory>;
+  getAllCategories(): Promise<IPackageCategory[]>;
+  createDestinationService(
+    data: Partial<IDestination> & {
+      latitude?: number;
+      longitude?: number;
+    },
+  ): Promise<IDestination>;
+  getAllDestinationsServise(): Promise<IDestination[]>;
+  getDestinationByIdService(id: string): Promise<IDestination>;
+  deleteDestinationByIdService(id: string): Promise<void>;
+  resetPasswordAuthenticatedService(
+    adminId: string,
+    oldPassword: string,
+    newPassword: string,
+    confirmPassword: string,
+  ): Promise<{
+    message: string;
+  }>;
+  updateUserService(id: string, data: any): Promise<IUser | null>;
+  getPaginatedPackagesService(skip: number, limit: number): Promise<Ipackage[]>;
+  getTotalPackagesCount(): Promise<number>;
+  updateOperatorService(id: string, data: any): Promise<IOperator | null>;
+  updateProfieImageService(id: string, image: string): Promise<IAdmin | null>;
+  deleteUserService(id: string): Promise<IUser | null>;
+}
---
+++ backend/services/adminService.ts
@@ -0,0 +1,233 @@
+import { IAdminRepository } from "../interfaces/IAdminRepository.js";
+import { IDestination } from "../models/Destination.js";
+import { IPackageCategory } from "../models/PackageCategory.js";
+import { CustomError } from "../utils/customError.js";
+
+import { IAdminService } from "../interfaces/IAdminService.js";
+import { IHashService } from "../interfaces/IHashService.js";
+import { IMailService } from "../interfaces/IMailService.js";
+import { ISecurityService } from "../interfaces/ISecurityService.js";
+import { ITokenService } from "../interfaces/ITokenService.js";
+import { IAdmin } from "../models/Admin.js";
+import { Ipackage } from "../models/Package.js";
+import { StatusCode } from "../constants/statusCodeConstants.js";
+
+export class AdminService implements IAdminService {
+  constructor(
+    private adminRepository: IAdminRepository,
+    private mailService: IMailService,
+    private hashService: IHashService,
+    private securityService: ISecurityService,
+    private tokenService: ITokenService,
+  ) {}
+
+  async loginAdminService(email: string, password: string) {
+    const admin = await this.adminRepository.findByEmail(email);
+    if (!admin) throw new CustomError("Admin not found", StatusCode.NOT_FOUND);
+    const isPasswordValid = this.hashService.compare(password, admin.password);
+    if (!isPasswordValid)
+      throw new CustomError("Invalid credentials", StatusCode.UNAUTHORIZED);
+    const accessToken = this.securityService.generateAccessToken({
+      id: admin._id.toString(),
+      role: admin.role,
+    });
+    const refreshToken = this.securityService.generateRefreshToken({
+      id: admin._id .toString(),
+      role: admin.role,
+    });
+    const { password: pass, ...adminData } = admin.toObject();
+    return { accessToken, refreshToken, adminData };
+  }
+
+  async updateAdminService(id: string, data: any) {
+    if (data.password) {
+      data.password = this.hashService.hash(data.password);
+    }
+    return await this.adminRepository.updateById(id, data);
+  }
+
+  async getOperatorVerificationRequestsService() {
+    return await this.adminRepository.getPendingOperator();
+  }
+
+  async verifyOperatorService(id: string, isVerified: boolean) {
+    const updated = await this.adminRepository.updateOperatorStatus(
+      id,
+      isVerified,
+    );
+    if (!updated)
+      throw new CustomError("Operator not found", StatusCode.NOT_FOUND);
+    const subject = "Verification Request update";
+
+    const message = isVerified
+      ? `Hi ${updated.name},<br><br>your operator account has been  <b>verified</b>.You can now access your dashboard and manage packages`
+      : `Hi ${updated.name},<br><br>your verification request has been  <b>rejected</b>.Please contact support for clarification`;
+    await this.mailService.sendEmail(updated.email, subject, message);
+    if (!isVerified) {
+      await this.adminRepository.deleteOperatorById(id);
+    }
+
+    return { message: `Operator ${isVerified ? "verified" : "rejected"}` };
+  }
+
+  async getPaginatedOperatorsService(skip: number, limit: number) {
+    return this.adminRepository.getPaginatedOperators(skip, limit);
+  }
+
+  getTotalOperatorsCount() {
+    return this.adminRepository.countAllOperators();
+  }
+
+  async getSingleOperatorService(id: string) {
+    return this.adminRepository.findOperatorById(id);
+  }
+
+  async blockOperatorService(id: string, isBlocked: boolean) {
+    return this.adminRepository.updateOperatorBlockStatus(id, isBlocked);
+  }
+
+  async deleteOperatorService(id: string) {
+    return this.adminRepository.deleteOperatorById(id);
+  }
+
+  async getPaginatedUsersService(skip: number, limit: number) {
+    return this.adminRepository.getPaginatedUsers(skip, limit);
+  }
+
+  getTotalUsersCount() {
+    return this.adminRepository.countAllUsers();
+  }
+
+  async getSingleUserService(id: string) {
+    return this.adminRepository.findUserById(id);
+  }
+
+  async blockUserService(id: string, isBlocked: boolean) {
+    return this.adminRepository.updateUserBlockStatus(id, isBlocked);
+  }
+
+  async createCategoryService(
+    data: Partial<IPackageCategory>,
+  ): Promise<IPackageCategory> {
+    if (!data.name)
+      throw new CustomError(
+        "Category name is required",
+        StatusCode.BAD_REQUEST,
+      );
+    const existing = await this.adminRepository.findPackageCategoryByName(
+      data.name,
+    );
+    if (existing)
+      throw new CustomError("Category already exists", StatusCode.BAD_REQUEST);
+    return this.adminRepository.createPackgeCategory(data);
+  }
+
+  getAllCategories() {
+    return this.adminRepository.findAllPackageCategory();
+  }
+
+  async createDestinationService(
+    data: Partial<IDestination> & { latitude?: number; longitude?: number },
+  ): Promise<IDestination> {
+    if (!data.name)
+      throw new CustomError(
+        "Destination name is required",
+        StatusCode.BAD_REQUEST,
+      );
+    if (data.latitude === null || data.longitude === null)
+      throw new CustomError(
+        "Latitude and longitude are required",
+        StatusCode.BAD_REQUEST,
+      );
+    const existing = await this.adminRepository.findDestinationByName(
+      data.name,
+    );
+    if (existing)
+      throw new CustomError(
+        "Destination with same name already exists",
+        StatusCode.BAD_REQUEST,
+      );
+
+    return this.adminRepository.createDestination({
+      name: data.name.trim(),
+      location: { latitude: data.latitude, longitude: data.longitude },
+      images: Array.isArray(data.images) ? data.images : [],
+    } as Partial<IDestination>);
+  }
+
+  getAllDestinationsServise(): Promise<IDestination[]> {
+    return this.adminRepository.findAllDestinations();
+  }
+
+  async getDestinationByIdService(id: string): Promise<IDestination> {
+    const destination = await this.adminRepository.findDestinationById(id);
+    if (!destination) {
+      throw new CustomError("Destination not found", StatusCode.NOT_FOUND);
+    }
+    return destination;
+  }
+
+  async deleteDestinationByIdService(id: string): Promise<void> {
+    const deleted = await this.adminRepository.deleteDestinationById(id);
+    if (!deleted)
+      throw new CustomError("Destination not found", StatusCode.NOT_FOUND);
+  }
+
+  async resetPasswordAuthenticatedService(
+    adminId: string,
+    oldPassword: string,
+    newPassword: string,
+    confirmPassword: string,
+  ) {
+    const admin = await this.adminRepository.findById(adminId);
+    if (!admin) throw new CustomError("Admin not found", StatusCode.NOT_FOUND);
+    const isMatch = await this.hashService.compare(oldPassword, admin.password);
+    if (!isMatch)
+      throw new CustomError(
+        "Old password is incorrect",
+        StatusCode.BAD_REQUEST,
+      );
+    if (confirmPassword !== newPassword)
+      throw new CustomError("Password do not match", StatusCode.BAD_REQUEST);
+    admin.password = this.hashService.hash(newPassword);
+    await this.adminRepository.save(admin);
+    return { message: "Password updated successfully" };
+  }
+
+  async deleteUserService(id: string) {
+    return await this.adminRepository.deleteUserById(id);
+  }
+  async updateUserService(id: string, data: any) {
+    if (data.password) {
+      data.password = this.hashService.hash(data.password);
+    }
+
+    return await this.adminRepository.updateUserById(id, data);
+  }
+
+  async getPaginatedPackagesService(
+    skip: number,
+    limit: number,
+  ): Promise<Ipackage[]> {
+    return this.adminRepository.findAllPackages(skip, limit);
+  }
+
+  getTotalPackagesCount() {
+    return this.adminRepository.countAllPackages();
+  }
+
+  async updateOperatorService(id: string, data: any) {
+    if (data.password) {
+      data.password = this.hashService.hash(data.password);
+    }
+
+    return await this.adminRepository.updateOperatorById(id, data);
+  }
+
+  async updateProfieImageService(
+    id: string,
+    image: string,
+  ): Promise<IAdmin | null> {
+    return this.adminRepository.updateProfieImage(id, image);
+  }
+}

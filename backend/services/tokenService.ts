20373bf608c93c7d3344eee672ec89a358fca553
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-21 15:01:21 +0530

    add token service

---
+++ backend/services/tokenService.ts
@@ -0,0 +1,17 @@
+import crypto from "crypto";
+import {
+  IResetTokenResponse,
+  ITokenService,
+} from "../interfaces/ITokenService.js";
+
+export class TokenService implements ITokenService {
+  getPasswordResetToken = (): IResetTokenResponse => {
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    const hashedToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+    const expireTime = Date.now() + 10 * 60 * 1000;
+    return { resetToken, hashedToken, expireTime };
+  };
+}

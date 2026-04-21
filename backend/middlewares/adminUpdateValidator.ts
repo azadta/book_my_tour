5aa2af860b87f058ba3dc3041f73f328777f1692
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-21 21:03:48 +0530

    add update admin validator

---
+++ backend/middlewares/adminUpdateValidator.ts
@@ -0,0 +1,63 @@
+import { body, validationResult, ValidationChain } from "express-validator";
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import { CustomError } from "../utils/customError.js";
+
+export const validateUpdateAdmin: (ValidationChain | RequestHandler)[] = [
+  body("name").optional().isString().withMessage("Name must be a string"),
+
+  body("email").optional().isEmail().withMessage("Invalid email address"),
+
+  body("password")
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage(
+      "Password must contain at least one special character (!@#$...)",
+    ),
+  body("image").optional().isURL().withMessage("Image must be a valid URL"),
+
+  body("address.houseNo")
+    .optional()
+    .isString()
+    .withMessage("House number must be a string"),
+
+  body("address.landmark")
+    .optional()
+    .isString()
+    .withMessage("Landmark must be a string"),
+
+  body("address.city")
+    .optional()
+    .isString()
+    .withMessage("City must be a string"),
+
+  body("address.state")
+    .optional()
+    .isString()
+    .withMessage("State must be a string"),
+
+  body("address.country")
+    .optional()
+    .isString()
+    .withMessage("Country must be a string"),
+
+  body("address.postalCode")
+    .optional({ checkFalsy: true })
+    .isPostalCode("any")
+    .withMessage("Invalid postal code"),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(new CustomError(errors.array()[0].msg, 400));
+    }
+    next();
+  },
+];

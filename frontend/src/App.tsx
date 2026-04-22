3f07cd44759ecb7d7baffdaece55fe7428637566
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-22 18:39:42 +0530

    add user state in header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/UserLayout";
 import Home from "./pages/user/Home";
+import { About } from "./pages/user/About";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route element={<UserLayout />}>
         <Route path="/" element={<Home/>}/>
+        <Route path="/user/about" element={<About/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import type { RootState } from "../redux/store";
 
 const Header = () => {
+  const { currentUser } = useSelector((state: RootState) => state.user);
   return (
     <div className="bg-slate-200 shadow-md w-full fixed">
       <div className="max-w-6xl mx-auto flex justify-between items-center p-6">
@@ -29,11 +32,26 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/user/login">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              Login
-            </li>
-          </Link>
+          {currentUser ? (
+            <Link to={"user/profile"}>
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.image}
+                alt={currentUser.name || "profile"}
+              />
+            </Link>
+          ) : (
+            <Link to="/user/login">
+              <li className="hidden sm:inline text-slate-700 hover:underline">
+                Login
+              </li>
+            </Link>
+          )}
+          {!currentUser && (
+            <Link to="/user/register">
+              <li className="text-slate-700 hover:under-line">Register</li>
+            </Link>
+          )}
         </ul>
       </div>
     </div>

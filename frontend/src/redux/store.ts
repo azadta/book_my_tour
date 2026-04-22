28d6b43dffb2b43b5db8757ca27342d03a36069b
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-22 16:37:48 +0530

    add operator slice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import operatorReducer from "./operator/operatorSlice";
 
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
@@ -20,6 +21,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
+  operator: operatorReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);

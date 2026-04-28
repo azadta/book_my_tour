ed391ddf1f468575dbb3b80a698fe8e89aaa75dc
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-28 10:14:55 +0530

    add ConfirmationModel component

---
+++ frontend/src/components/ConfirmationModel.tsx
@@ -0,0 +1,39 @@
+interface ConfirmationModelProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  message: string;
+}
+
+const ConfirmationModel = ({
+  isOpen,
+  message,
+  onClose,
+  onConfirm,
+}: ConfirmationModelProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50  flex items-center justify-center bg-black/50">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm ">
+        <p className="text-gray-800 text-lg mb-4">{message}</p>
+        <div className="flex justify-end space-x-3">
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400  "
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onConfirm}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600  "
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModel;
--- frontend/src/pages/admin/AdminUserDetails.tsx
+++ frontend/src/pages/admin/AdminUserDetails.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import ConfirmationModel from "../../components/ConfirmationModel";
+
 const AdminUserDetails = () => {
-  return <div></div>;
+   const [modalOpen, setModalOpen] = useState(true);
+  const [modalMessage, setModalMessage] = useState("how are you");
+  const [modalAction, setModalAction] = useState<() => void>(() => () => {});
+  return <div>
+    <ConfirmationModel isOpen={modalOpen}
+        message={modalMessage}
+        onClose={() => setModalOpen(false)}
+        onConfirm={() => {
+          modalAction();
+          setModalOpen(false);
+        }}/>
+  </div>;
 };
 
 export default AdminUserDetails;

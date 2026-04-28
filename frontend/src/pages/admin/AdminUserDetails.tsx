295beb839026acb3d94b77db1d345a7de14bc5df
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-04-28 11:09:21 +0530

    add Pagination component

--- frontend/src/pages/admin/AdminUserDetails.tsx
+++ frontend/src/pages/admin/AdminUserDetails.tsx
@@ -1,19 +1,5 @@
-import { useState } from "react";
-import ConfirmationModel from "../../components/ConfirmationModel";
-
 const AdminUserDetails = () => {
-   const [modalOpen, setModalOpen] = useState(true);
-  const [modalMessage, setModalMessage] = useState("how are you");
-  const [modalAction, setModalAction] = useState<() => void>(() => () => {});
-  return <div>
-    <ConfirmationModel isOpen={modalOpen}
-        message={modalMessage}
-        onClose={() => setModalOpen(false)}
-        onConfirm={() => {
-          modalAction();
-          setModalOpen(false);
-        }}/>
-  </div>;
+  return <div></div>;
 };
 
 export default AdminUserDetails;

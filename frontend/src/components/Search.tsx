1ef8e5817165a3641a24f4bf3148dd79fe5f7970
Abdul Karim Azad <abdulkareemazad@gmail.com>
2026-06-05 17:15:16 +0530

    add search ui

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Search = () => {
-  return <div></div>;
+  const [showSearch, setShowSearch] = useState(true);
+  useEffect(() => {
+    const scrollHandler = () => {
+      setShowSearch(window.scrollY < 150);
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+  return (
+    <div
+      className={`bg-sky-50 pb-2 -mt-20 transition-all w-full duration-300 fixed z-50 left-1/2 -translate-x-1/2  ${showSearch ? "hidden md:block" : "hidden"}`}
+    >
+      <div className="flex justify-center items-center text-sky-400 gap-2 pb-2">
+        Search By <IoSearchSharp />
+      </div>
+      <div className="flex items-center justify-center gap-1 lg:gap-3 px-1">
+        <div className="border px-8 bg-white border-amber-400">Country</div>
+        <div className="border px-8 bg-white border-amber-400">Budget</div>
+        <div className="border px-8 bg-white border-amber-400">Nature</div>
+        <div className="border px-8 bg-white border-amber-400">Adventure</div>
+        <div className="border px-8 bg-white border-amber-400">Destination</div>
+        <div className="border px-8 bg-white border-amber-400">Date</div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
